85b07d8e88b45c9ed64365e75a0c9a8dd7273067
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2022-06-23 18:59:03 -0400

    shopping cart

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,6 +2,9 @@ import * as React from "react"
 import "./ShoppingCart.css"
 
 export default function ShoppingCart({isOpen, shoppingCart, product}){
+    let subTotal = 0 
+    let taxes = 0
+    let total = 0
     return (
             <div className="open">
               <h3 className="">Shopping Cart <span className="button"><i className="material-icons md-48">add_shopping_cart</i></span></h3>
@@ -14,19 +17,43 @@ export default function ShoppingCart({isOpen, shoppingCart, product}){
                         <span className="center">Unit Price</span>
                         <span className="center">Cost</span>
                     </div>
-                </div>
-                {shoppingCart.map((item) => {
+                    {shoppingCart.map((item) => {
                     let name = product.find(element => element.id === item.itemId).name
                     let quantity = item.quantity
                     let price = product.find(element => element.id === item.itemId).price.toFixed(2)
                     let cost = (quantity*price).toFixed(2)
+                    subTotal += (quantity*price)
+                    taxes = 0.0875 * subTotal
+                    total = subTotal + taxes
                     return (<div className="product-row">
                                 <span className="flex-2 cart-product-name">{name}</span>
                                 <span className="center cart-product-quantity">{quantity}</span>
                                 <span className="center cart-product-price">{"$"+price}</span>
-                                <span className="center cart-product-subtotal">{cost}</span>
+                                <span className="center cart-product-subtotal">{"$"+cost}</span>
                             </div>)
-                })}
+                    })}
+                </div>
+                <div className="receipt">
+                    <div className="receipt-subtotal">
+                        <span className="label">Subtotal</span>
+                        <span></span>
+                        <span></span>
+                        <span className="center subtotal">{"$"+subTotal.toFixed(2)}</span>
+                    </div>
+                    <div class="receipt-taxes">
+                        <span class="label">Taxes and Fees</span>
+                        <span></span>
+                        <span></span>
+                        <span class="center">{"$"+taxes.toFixed(2)}</span>
+                    </div>
+                    <div class="receipt-total">
+                        <span class="label">Total</span>
+                        <span></span>
+                        <span></span>
+                        <span class="center total-price">{"$"+total.toFixed(2)}</span>
+                    </div>
+                </div>
+
               </div>
               : 
               <div className="notification">No items added to cart yet. Start shopping now!</div>}
